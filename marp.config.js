1c0de193dc0f633f777cf2651e5230607c9aca72
tosi <tosi16@gmail.com>
2025-11-12 20:03:51 +0900

    Fix PDF generation settings in marp.config.js
    Updated PDF generation options in Marp configuration.

--- marp.config.js
+++ marp.config.js
@@ -3,11 +3,9 @@ module.exports = {
   output: 'docs',
   allowLocalFiles: true,
   html: true,
-  puppeteer: {
-    launchOptions: {
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    },
-  },
+  options: {
+    pdfChromiumArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },  
   themeSet: [
     'themes/brand.css',
     'slides/alice/hello-world/_theme/hello-world.css',
